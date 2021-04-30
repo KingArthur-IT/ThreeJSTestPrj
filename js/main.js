d76c02f4a72d4e64f37131fbb85d34460084bcd5
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-04-30 17:53:27 +0300

    проба прозрачной текстуры

--- js/main.js
+++ js/main.js
@@ -19,16 +19,24 @@ window.onload = function () {
     let light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
 
-    const geometry = new THREE.PlaneGeometry(300.0, 300.0, 300.0);
+    const geometry = new THREE.PlaneGeometry(300.0, 300.0, 10.0);
+    const geometry2 = new THREE.PlaneGeometry(300.0, 300.0, 10.0);
     const loader = new THREE.TextureLoader();
-     const material = new THREE.MeshBasicMaterial({
-            //map: loader.load('https://threejsfundamentals.org/threejs/resources/images/wall.jpg'),
-            map: loader.load('textures.jpg'),
+    const material = new THREE.MeshBasicMaterial({
+            map: loader.load('https://threejsfundamentals.org/threejs/resources/images/wall.jpg'),
+            //map: loader.load('textures.jpg'),
+    });
+    const material2 = new THREE.MeshBasicMaterial({
+            //map: loader.load('https://webglfundamentals.org/webgl/resources/leaves.jpg'),
+            map: loader.load('pattern.png'),
      });
 
 
     let mesh = new THREE.Mesh(geometry, material);
+    let mesh2 = new THREE.Mesh(geometry2, material2);
+    mesh2.position.z += 150;
     scene.add(mesh);
+    scene.add(mesh2);
 
     function loop() {
         //mesh.rotation.y += Math.PI / 100;
